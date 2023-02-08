bd451865909c4388f33d46be8554625ec0bdef6e
Ian Namie <Iannamie82@gmail.com>
2023-02-08 14:42:35 -0600

    finished first exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I want to play a game! guess my favorite number!");
+            int favNumber = 82;
+            int userGuess = int.Parse(Console.ReadLine()); 
+
+            if (userGuess == favNumber) 
+            {
+                Console.WriteLine("That is correct great job!");
+            }
+            else if (userGuess < favNumber)
+            {
+                Console.WriteLine("Aww close but thats to low!");
+            }
+            else 
+            { 
+                Console.WriteLine("Aww good guess but thats to high!");
+            }
+
+
         }
     }
 }
